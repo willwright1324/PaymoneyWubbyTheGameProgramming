c6a196297e7f2071c450b03f5ddfce6631ef4b87
William Wright <william_wright@bloomfield.edu>
2021-02-14 00:40:03 -0500

    initial commit

---
+++ MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+    public GameObject confirmation;
+
+    void Start() {
+        GlobalController.Instance.Init();
+        GlobalController.Instance.canPause = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void MenuPlay() {
+        GlobalController.Instance.MenuPlay();
+        GlobalController.Instance.canPause = true;
+    }
+    public void QuitGame() {
+        GlobalController.Instance.QuitGame();
+    }
+    public void DeleteSave() {
+        confirmation.SetActive(true);
+    }
+    public void YesDelete() {
+        GlobalController.Instance.DeleteSave();
+        confirmation.SetActive(false);
+    }
+    public void NoDelete() {
+        confirmation.SetActive(false);
+    }
+}
---
+++ QuestObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour {
+    public ChainTrigger trigger;
+
+    void Awake() {
+        GlobalController.Instance.QuestTrackerScript.objects.Add(gameObject);
+        gameObject.SetActive(trigger.questTriggered);
+    }
+    void Start() {}
+}
